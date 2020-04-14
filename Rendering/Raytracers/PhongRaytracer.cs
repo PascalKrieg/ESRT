0ce2566ead5fd9600afbf98a2f0e6c7b4a4f97c6
Pascal Krieg <pascalkrieg@aol.com>
2020-04-14 15:54:50 +0200

    Fixed the CastShadows setting

--- Rendering/Raytracers/PhongRaytracer.cs
+++ Rendering/Raytracers/PhongRaytracer.cs
@@ -21,25 +21,22 @@ namespace ESRT.Rendering
 
         protected override Color calculateColorPerLight(Ray ray, HitData hitPoint, ILight light, bool isCovered)
         {
-            if (!Settings.CastShadows)
-                return Color.Black;
-
-            Vector3 lightDirection = Vector3.Normalize(hitPoint.Position - light.Position);
-            Color lightIntensity = (float)(1 / Math.Pow(Vector3.Distance(hitPoint.Position, light.Position), 2)) * light.GetIntensity(lightDirection);
+            Vector3 incomingLightDirection = Vector3.Normalize(hitPoint.Position - light.Position);
+            Color lightIntensity = (float)(1 / Math.Pow(Vector3.Distance(hitPoint.Position, light.Position), 2)) * light.GetIntensity(incomingLightDirection);
 
             Color ambient = lightIntensity * hitPoint.Material.Ambient(hitPoint.TextureCoords);
             Color diffuse = Color.Black;
             Color specular = Color.Black;
 
-            if (!isCovered)
+            if (!isCovered && Settings.CastShadows)
             {
                 diffuse = lightIntensity
-                    * Math.Max(hitPoint.Normal * (-1 * lightDirection), 0)
+                    * Math.Max(hitPoint.Normal * (-1 * incomingLightDirection), 0)
                     * hitPoint.Material.Diffuse(hitPoint.TextureCoords);
 
                 specular = lightIntensity
                     * hitPoint.Material.Specular(hitPoint.TextureCoords)
-                    * (float)Math.Pow((-1 * CurrentScene.MainCamera.ViewDirection).CosAngle(lightDirection.Reflect(hitPoint.Normal)), PhongExponent);
+                    * (float)Math.Pow(-1 * CurrentScene.MainCamera.ViewDirection * incomingLightDirection.Reflect(hitPoint.Normal), PhongExponent);
 
             }
 
