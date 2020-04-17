3a08af753389a506d8a5a5a88593752318ee9cbb
Pascal Krieg <pascalkrieg@aol.com>
2020-04-17 20:06:53 +0200

    Added MathUtility and Matrix3x3 Classes that will be used in polygon intersection tests

---
+++ Utility/MathUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESRT
+{
+    public static class MathUtility
+    {
+        public static (float a, float b, float c) SolveLinearEquationSystem(Matrix3x3 matrix, Vector3 equals)
+        {
+            float determinant = matrix.Determinant;
+            (float a, float b, float c) result;
+            result.a = matrix.ExchangeColumn(0, equals).Determinant / determinant;
+            result.b = matrix.ExchangeColumn(1, equals).Determinant / determinant;
+            result.c = matrix.ExchangeColumn(2, equals).Determinant / determinant;
+            return result;
+        }
+    }
+}
