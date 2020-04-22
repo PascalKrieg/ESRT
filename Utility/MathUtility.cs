c1f38d74042ca12fb80588fd16d2cb5ac37ef00c
Pascal Krieg <pascalkrieg@aol.com>
2020-04-22 16:35:01 +0200

    Added documentation
    Added documentation to MathUtility

--- Utility/MathUtility.cs
+++ Utility/MathUtility.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace ESRT
 {
+    /// <summary>
+    /// Utility class for various math related tasks.
+    /// </summary>
     public static class MathUtility
     {
+        /// <summary>
+        /// Solves a linear equation in the form A * x = v, with A being a 3x3 matrix and v being a given vector. 
+        /// </summary>
+        /// <param name="matrix">The 3x3 matrix A.</param>
+        /// <param name="equals">The Vector v.</param>
+        /// <returns></returns>
         public static (float a, float b, float c) SolveLinearEquationSystem(Matrix3x3 matrix, Vector3 equals)
         {
             float determinant = matrix.Determinant;
